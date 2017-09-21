6e67363b810814fea97f6bbefa7d64d5b92be2e8
Phillip Luther <phil@phillipluther.com>
2017-09-20 23:54:47 -0700

    Merge in-progress to master (#2)
    * Added documentation mini-site, 
    * Shaved ~60 bytes
    * BREAKING: renamed .on and .off to .onChange and .offChange; updated documentation to reflect changes; new builds in ./lib

--- src/jetSet.js
+++ src/jetSet.js
@@ -1,69 +1,61 @@
+
+let Obj = Object;
+
 //
 //
-function _val(val) {
+function _eval(val) {
     return (typeof val === 'function') ? val() : val;
 }
 
 //
 //
-function createStore(defaults) {
-
-    function Store() {
-        for (let prop in defaults) {
-            this[prop] = defaults[prop];
-        }
-    }
-
-    let prototypeHooks = {
-        watchers: {},
+function createStore() {
+    return Obj.create({
         snapshot: {},
+        watchers: {},
 
         //
         //
-        on: function (prop, action) {
+        onChange: function (prop, action) {
             let $this = this;
             let watchers = $this.watchers;
 
             watchers[prop] = watchers[prop] || [];
             watchers[prop].push(action);
-            $this.snapshot[prop] = _val($this[prop]);
+            $this.snapshot[prop] = _eval($this[prop]);
         },
         //
         //
-        off: function (prop, actionToRemove) {
+        offChange: function (prop, actionToRemove) {
             let watchers = this.watchers;
             watchers[prop] = watchers[prop].filter(
                 (action) => action !== actionToRemove
             );
         },
-    };
-
-    for (let hook in prototypeHooks) {
-        Store.prototype[hook] = prototypeHooks[hook];
-    }
-
-    return new Store();
+    });
 }
 
 //
 //
 function jetSet(defaults) {
-    let store = createStore(defaults);
+    let store = createStore();
+    Obj.assign(store, defaults);
 
     return new Proxy(store, {
         set: (store, prop, val) => {
+            let watchers = store.watchers;
+            let snapshot = store.snapshot;
+
             store[prop] = val;
 
-            let watchers = store.watchers;
-
             for (let prop in watchers) {
-                let newVal = _val(store[prop]);
+                let oldVal = snapshot[prop];
+                let newVal = _eval(store[prop]);
                 let actions = watchers[prop];
-                let oldVal = store.snapshot[prop];
 
                 if (newVal !== oldVal) {
                     actions.map((action) => action(newVal, oldVal));
-                    store.snapshot[prop] = newVal;
+                    snapshot[prop] = newVal;
                 }
             }
 
@@ -71,7 +63,7 @@ function jetSet(defaults) {
         },
         //
         get: (store, prop) => {
-            return store.hasOwnProperty(prop) ? _val(store[prop]) : store[prop];
+            return store.hasOwnProperty(prop) ? _eval(store[prop]) : store[prop];
         }
     });
 }
--- tests/on.test.js
+++ tests/on.test.js
@@ -1,23 +1,23 @@
 let expect = require('chai').expect;
 let jetSet = require('../src/jetSet');
 
-describe('Observing (state.on)', () => {
+describe('Observing (state.onChange)', () => {
     let state;
     beforeEach(() => state = jetSet());
 
     it('should be available on an initialized store', () => {
-        expect(state.on).not.to.be.undefined;
+        expect(state.onChange).not.to.be.undefined;
     });
 
     it('should accept a callback action for a given prop (change)', () => {
-        state.on('myProp', () => 1 + 1);
+        state.onChange('myProp', () => 1 + 1);
         // expect no exceptions
     });
 
     it('should provide new and old value params to a given action', () => {
         let newValCheck, oldValCheck;
         state.myProp = 'OLD';
-        state.on('myProp', (newVal, oldVal) => {
+        state.onChange('myProp', (newVal, oldVal) => {
             newValCheck = newVal;
             oldValCheck = oldVal;
         });
@@ -38,21 +38,21 @@ describe('Observing (state.on)', () => {
         let context = new fauxClass();
         let action = function() { return true; };
 
-        state.on('changeMe', action.bind(context));
+        state.onChange('changeMe', action.bind(context));
         state.changeMe = 'OK';
 
         expect(thisCheck).to.equal('fauxClass');
     });
 
     it('should update state.prototype.watchers for managing observed props', () => {
-        state.on('changeMe', () => 1 + 1);
+        state.onChange('changeMe', () => 1 + 1);
         expect(state.watchers.changeMe).not.to.be.undefined;
         expect(state.watchers.changeMe.length).to.equal(1);
     });
 
     it('should stack actions to a single observed property', () => {
-        state.on('changeMe', () => 1 + 1);
-        state.on('changeMe', () => 2 + 2);
+        state.onChange('changeMe', () => 1 + 1);
+        state.onChange('changeMe', () => 2 + 2);
 
         expect(state.watchers.changeMe.length).to.equal(2);
     });
